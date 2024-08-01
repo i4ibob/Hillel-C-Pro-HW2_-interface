759883246645c8442aecdc5c95e3ce16354f6d64
i4ibob <i4ibob@gmail.com>
2024-08-01 15:55:25 +0300

    Add project files.

---
+++ Hillel C#Pro HW2_ interface/Program.cs
@@ -0,0 +1,47 @@
+﻿//Створіть інтерфейс IMovable
+interface IMovable
+{
+    // з методом void Move(int deltaX, int deltaY)
+    void Move(int deltaX, int deltaY)
+    {
+
+    }
+}
+
+abstract class Shape : IMovable
+{
+
+}
+
+//Реалізуйте метод Move в класах Circle і Rectangle.
+class Circle : IMovable
+{
+
+}
+
+class Rectangle
+{
+
+}
+
+//Створіть структуру Vector з властивостями double X і double Y, додайте метод для обчислення довжини вектора.
+struct Vector
+{
+
+}
+
+public enum ProgramFunc
+{
+    ShowInfo = 1,
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+    }
+}
